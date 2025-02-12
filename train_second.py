c2c385eba92359b9ab864f065a85fd71639ef734
Jindrich Matousek <jindrich.matousek@gmail.com>
2025-02-12 15:18:31 +0100

    Update comments in train_second.py to clarify tensor squeezing behavior

--- train_second.py
+++ train_second.py
@@ -818,7 +818,8 @@ def main():
                     # otherwise all dimensions of size 1 are removed
                     # (resulting in error when current batch size is 1)
                     s = torch.stack(ss).squeeze()
-                    # s = torch.stack(ss).squeeze(dim=-1)
+                    # s = torch.stack(ss).squeeze(dim=-1) - not working
+                    # s = torch.stack(ss).squeeze(dim=1) ???
                     # gs = torch.stack(gs).squeeze()              # !!! JMa: not used anymore?
                     # # gs = torch.stack(gs).squeeze(dim=-1)        # !!! JMa: not used anymore?
                     # s_trg = torch.cat([s, gs], dim=-1).detach() # !!! JMa: not used anymore?
