28214c7a5383de94c8ab3e40372ca3ece5032c0b
Jindrich Matousek <jindrich.matousek@gmail.com>
2025-05-28 12:37:48 +0200

    Update config generation: set second_stage_load_pretrained flag for various stages

--- Bin/prep_configs.py
+++ Bin/prep_configs.py
@@ -56,6 +56,7 @@ def main():
     config_out["grad_accum_steps"] = settings["start1"]["grad_accum_steps"]
     config_out["max_len"] = settings["start1"]["max_len"]
     config_out["data_params"]["train_data"] = settings["start1"]["train_data"]
+    config_out["second_stage_load_pretrained"] = False
     out_file = osp.join(out_dir, f"{cfg_name}{settings['start1']['label']}{cfg_ext}")
     save_config(config_out, out_file)
 
@@ -68,6 +69,7 @@ def main():
     config_out["data_params"]["train_data"] = settings["tma"]["train_data"]
     pretrained_model = osp.join(out_dir, f"epoch_1st_{config['epochs']['tma']-1:05}.pth")
     config_out["pretrained_model"] = pretrained_model
+    config_out["second_stage_load_pretrained"] = False
     out_file = osp.join(out_dir, f"{cfg_name}{settings['tma']['label']}{cfg_ext}")
     save_config(config_out, out_file)
 
@@ -79,6 +81,7 @@ def main():
     config_out["batch_size"] = settings["start2"]["batch_size"]
     config_out["max_len"] = settings["start2"]["max_len"]
     config_out["data_params"]["train_data"] = settings["start2"]["train_data"]
+    config_out["second_stage_load_pretrained"] = True
     out_file = osp.join(out_dir, f"{cfg_name}{settings['start2']['label']}{cfg_ext}")
     save_config(config_out, out_file)
 
@@ -88,8 +91,10 @@ def main():
     config_out["batch_size"] = settings["diff"]["batch_size"]
     config_out["max_len"] = settings["diff"]["max_len"]
     config_out["data_params"]["train_data"] = settings["diff"]["train_data"]
+    config_out["second_stage_load_pretrained"] = "true"
     pretrained_model = osp.join(out_dir, f"epoch_2nd_{config['epochs']['diff']-1:05}.pth")
     config_out["pretrained_model"] = pretrained_model
+    config_out["second_stage_load_pretrained"] = True
     out_file = osp.join(out_dir, f"{cfg_name}{settings['diff']['label']}{cfg_ext}")
     save_config(config_out, out_file)
 
@@ -101,6 +106,7 @@ def main():
     config_out["data_params"]["train_data"] = settings["joint"]["train_data"]
     pretrained_model = osp.join(out_dir, f"epoch_2nd_{config['epochs']['joint']-1:05}.pth")
     config_out["pretrained_model"] = pretrained_model
+    config_out["second_stage_load_pretrained"] = True
     out_file = osp.join(out_dir, f"{cfg_name}{settings['joint']['label']}{cfg_ext}")
     save_config(config_out, out_file)
 
