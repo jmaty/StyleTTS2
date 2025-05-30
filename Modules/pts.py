15255807a9b42c9511258b37c35a48ed41969d72
Jindrich Matousek <jindrich.matousek@gmail.com>
2025-05-30 14:30:13 +0200

    Update acoustic style encoding to include mel spectrogram input

--- Modules/pts.py
+++ Modules/pts.py
@@ -574,7 +574,7 @@ class PTS:
                 n = log_norm(mel_gt.unsqueeze(1)).squeeze(1)
 
             # Encode style from ground truth mel spectrogram
-            acoust_style = self.model.acoustic_style_encoder(spk_emb)
+            acoust_style = self.model.acoustic_style_encoder(mel_gt.unsqueeze(1), spk_emb)
             # pros_style = self.model.prosodic_style_encoder(mel_gt.unsqueeze(1))
             # style = torch.cat([acoust_style, pros_style], dim=1)
             # Decode
