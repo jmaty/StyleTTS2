6c479aac8c5e5703b50ffd2d4026cb28519ebc64
Jindrich Matousek <jindrich.matousek@gmail.com>
2025-07-21 16:17:12 +0200

    Refactor main function: store acoustic style dimension in a variable

--- train_first.py
+++ train_first.py
@@ -172,6 +172,7 @@ def main():
         plbert = load_plbert(bert_path)
 
     model = build_model(model_params, text_aligner, pitch_extractor, plbert)
+    acoustic_style_dim = model.acoustic_style_encoder.style_dim
     bert_size = model.bert.config.max_position_embeddings  # ALBERT config
 
     for k in model:
@@ -273,7 +274,7 @@ def main():
             start_epoch = 0
             iters = 0
         # Log model parameters
-        logger.info("Acoustic style dimension: %d", model.acoustic_style_encoder.style_dim)
+        logger.info("Acoustic style dimension: %d", acoustic_style_dim)
         logger.info("")
 
     # in case not distributed computing
